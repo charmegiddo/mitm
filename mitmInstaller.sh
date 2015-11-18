220ee3e8e09725471d46b05fc3cf16f67aa07dfb
hshintaro <hshintaro575@gmail.com>
2015-11-18 22:40:53 +0900

    HarMegiddo@Mac-Book-Air commit:[2015/11/18, 10:40:53 PM]

---
+++ mitmInstaller.sh
@@ -0,0 +1,51 @@
+#!/bin/bash
+echo "#####################################################################
+#     #   ###   ####### #     #
+##   ##    #       #    ##   ##
+# # # #    #       #    # # # #
+#  #  #    #       #    #  #  #
+#     #    #       #    #     #
+#     #    #       #    #     #
+#     #   ###      #    #     #
+
+###   #     #  #####  #######    #    #       #       ####### ######
+ #    ##    # #     #    #      # #   #       #       #       #     #
+ #    # #   # #          #     #   #  #       #       #       #     #
+ #    #  #  #  #####     #    #     # #       #       #####   ######
+ #    #   # #       #    #    ####### #       #       #       #   #
+ #    #    ## #     #    #    #     # #       #       #       #    #
+###   #     #  #####     #    #     # ####### ####### ####### #     #
+#####################################################################"
+
+echo "Require Environment: RASPBIAN WHEEZY ver 3.18, Date: May 2015
+"
+
+while true
+do
+    echo -n "python-pip, python-dev, libxml2-dev, libxslt-dev, 
+libffi-dev, python-lxml, dsniff, libjpeg-dev, libfreetype6,
+libfreetype6-dev, zlib1g-dev, mitmproxy
+Install the above for this machine use the mitmproxy and arpspoof [y/n] :"
+    read ANS
+    if [ "${ANS}" = 'y' -o "${ANS}" = 'yes' ]; then
+        break
+    elif [ "${ANS}" = 'n' -o "${ANS}" = 'no' ]; then
+        exit 1
+    else
+        continue 1
+    fi
+done
+
+sudo apt-get -y update 
+sudo apt-get -y upgrade
+sudo apt-get -y dist-upgrade
+wget https://raw.github.com/pypa/pip/master/contrib/get-pip.py
+chmod +x get-pip.py
+sudo ./get-pip.py
+sudo apt-get -y install python-pip
+sudo apt-get install -y python-dev libxml2-dev libxslt-dev libffi-dev python-lxml
+sudo apt-get -y install libjpeg-dev libfreetype6 libfreetype6-dev zlib1g-dev
+sudo apt-get install -y dsniff
+sudo pip install mitmproxy
+
+echo "Done."
