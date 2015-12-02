4887d2efe6b20e7effc32d8f320fb35df71f244b
hshintaro <hshintaro575@gmail.com>
2015-12-02 13:56:59 +0900

    HarMegiddo@Mac-Book-Air commit:[2015/12/02, 01:56:59 PM]

--- change-iptables.sh
+++ change-iptables.sh
@@ -1,1 +1,28 @@
-#!/bin/bash
+#!/bin/sh
+i=0
+
+while true
+do
+  R=`tail -n 1 $1 | sed -e 's/ //g'`
+
+  if [ $(echo $R | grep -e "ABCDEFGHIJKLMN") ]; then
+    continue
+  fi
+
+  if [ $i -eq "2" ]; then
+    echo "exec"
+    iptables -t nat -A PREROUTING -i wlan0 -p tcp --dport 443 -j REDIRECT --to-port 8080
+  fi
+
+  if [ $(echo $R | grep -e "KEY WORD") ]; then
+    i=`expr $i + 1`
+    echo "ABCDEFGHIJKLMN" >>  $1
+  else
+    i=0
+  fi
+
+done
+
+
+# sudo iptables -t nat -L --line-numbers
+# sudo iptables -t nat -D PREROUTING [N]
